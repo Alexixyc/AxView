85c3eeb0e79eb2923e48c617bb326baa959767c7
Alexixyc <Alexixyc@outlook.com>
2017-07-28 16:46:04 +0800

    分页组件实现中...

--- my-components/src/components/pagination/pagination.vue
+++ my-components/src/components/pagination/pagination.vue
@@ -2,7 +2,7 @@
 	<div class="main-box">分页组件
 		
 		<span class="page-block"> < </span>
-		<span v-for = "(item,index) in pageIndex" class="page-block">{{item}}</span>
+		<span v-for = "(item,index) in indexArr" class="page-block">{{item}}</span>
 		<span class="page-block"> > </span>
 
 	</div>
@@ -13,18 +13,21 @@
 module.exports = {
 	data: function() {
 		return {
-			pageIndex:[1,2,3,4,5,6],
-			totalNum:'100',
-			numPerPage:'11',
-			pageAmount:''
+			indexArr: [], //
+			totalNum: '100', //数据项的总数
+			numPerPage: '8', //每页数据项数目
+			pageAmount: '' //分页后，总的页码数
 		}
 	},
-	mounted:function(){
-		
+	mounted: function() {
+		var that = this;
+		var y = that.totalNum % that.numPerPage;//总条目数 除以 每页条目数的余数
+		var s = Math.floor(that.totalNum / that.numPerPage);//总条目数 除以 每页条目数的商
+		that.pageAmount = (y === 0) ? s : (s + 1);
+		for (var i = 0; i < that.pageAmount; i++) {
+			that.indexArr.push(i + 1);
+		}
 	}
-
-
-
 }
 
 </script>
